e584104b155b07a74efbcabd30b3bcba25cdc065
fufaich <fufaich@gmail.com>
2023-07-02 14:08:01 +0300

    Final

--- SomeSort.h
+++ SomeSort.h
@@ -4,28 +4,53 @@ template <typename T>
 void Merge(T* begin, T* mid, T* end){
     if((begin == mid) && (*begin > *end)){
         T tmp = *begin;
-        begin = *end;
-        end = tmp;
+        *begin = *end;
+        *end = tmp;
     }
     else{
         T* rightTapePtr = mid +1;
         T* leftTapePtr = begin;
         std::vector<T> firtsTape;
-        while (rightTapePtr <= mid)
+        while (rightTapePtr <= end)
         {
             firtsTape.push_back(*rightTapePtr);
             rightTapePtr++;
         }
 
         std::vector<T> secondTape;
-        while (leftTapePtr <= end)
+        while (leftTapePtr <= mid)
         {
             secondTape.push_back(*leftTapePtr);
             leftTapePtr++;
         }
 
         while(!firtsTape.empty() || !secondTape.empty()){
-            
+            if(firtsTape.empty()){
+                *begin = secondTape.front();
+                secondTape.erase(secondTape.begin()); 
+                begin++;
+                continue;
+            }
+
+            if(secondTape.empty()){
+                *begin = firtsTape.front();
+                firtsTape.erase(firtsTape.begin()); 
+                begin++;
+                continue;
+            }
+            T leftValue = firtsTape.front();
+            T rightValue = secondTape.front();
+
+            if(leftValue < rightValue){
+                *begin = firtsTape.front();
+                firtsTape.erase(firtsTape.begin()); 
+                begin++;
+            }
+            else{
+                *begin = secondTape.front();
+                secondTape.erase(secondTape.begin()); 
+                begin++;
+            }
         }
 
         
--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include "SomeSort.h"
+
+template <typename T, size_t N>
+void printList(const T (&list)[N]) {
+    std::cout << "List: [";
+    for (size_t i = 0; i < N; i++) {
+        std::cout << list[i];
+        if (i != N - 1)
+            std::cout << ", ";
+    }
+    std::cout << "]" << std::endl;
+}
 
 int main(){
-    std::cout<< "Hello" << std::endl;
+    int list[] = {5,13,166,8,6,1,2,29};
+    std:: cout << "Before" << std:: endl;
+    printList(list);
+    SomeSort(&list[0],&list[7]);
+    std:: cout << "After" << std:: endl;
+    printList(list);
 
     return 0;
 }
